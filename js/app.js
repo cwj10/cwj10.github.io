21b4dbf0aea8634ff5eccc8a22aee71ab84d365e
chenwenjie <798593032@qq.com>
2015-04-17 15:00:07 +0800

    home page

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 /**
  * Created by chenwenjie on 2015/4/17.
  */
-angular.module('cwj-starter',['ui.router'])
+angular.module('cwj-starter',['ui.router','cwj-controllers'])
 .config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
         $stateProvider.
             state('home', {
                 url:'/home',
-                templateUrl:'templates/home.html'
+                templateUrl:'templates/home.html',
+                controller:'HomeController'
             })
         $urlRouterProvider.otherwise("/home");
 }])
