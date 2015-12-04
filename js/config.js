715fae810e29565e0ce6afbf1ef2dc939f3746a2
cwj10 <798593032@qq.com>
2015-12-04 18:07:14 +0800

    blog

--- js/config.js
+++ js/config.js
@@ -1,7 +1,12 @@
 /**
  * Created by cwj on 2015/11/19.
  */
-var data = [
-    {'name':'aa','age':18},
-    {'name':'bb','age':22}
-]
+ var ARTICLES = [
+     {'id':1,'title':'屏幕适配1','description':'屏幕适配','time':'2015-12-04'},
+     {'id':2,'title':'屏幕适配2','description':'屏幕适配','time':'2015-12-04'},
+     {'id':3,'title':'屏幕适配3','description':'屏幕适配','time':'2015-12-04'},
+     {'id':4,'title':'屏幕适配4','description':'屏幕适配','time':'2015-12-04'},
+     {'id':5,'title':'屏幕适配5','description':'屏幕适配','time':'2015-12-04'},
+     {'id':6,'title':'屏幕适配6','description':'屏幕适配','time':'2015-12-04'},
+     {'id':7,'title':'屏幕适配7','description':'屏幕适配','time':'2015-12-04'}
+ ];
--- js/utils.js
+++ js/utils.js
@@ -13,3 +13,10 @@ var loadData = function(name){
         }
     });
 }
+
+function GetQueryString(name)
+{
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);
+     if(r!=null)return  unescape(r[2]); return null;
+}
