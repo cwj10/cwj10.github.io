9e7d4a3080f9a401f944aee20060e285df760f96
cwj10 <798593032@qq.com>
2016-02-15 17:30:42 +0800

    animation

--- js/config.js
+++ js/config.js
@@ -12,7 +12,9 @@
    {'id':8,'title':'屏幕适配','description':'屏幕适配','time':'2015-12-08','path':'android/屏幕适配.md'},
    {'id':9,'title':'Android常用网络框架比较','description':'Android常用网络框架比较','time':'2015-12-10','path':'android/Android常用网络框架比较.md'},
    {'id':10,'title':'git常用命令','description':'git常用命令','time':'2016-02-14','path':'android/git常用命令.md'},
-   {'id':11,'title':'AsyncTask和Handler的比较','description':'AsyncTask和Handler的比较','time':'2016-02-15','path':'android/AsyncTask和Handler的比较.md'}
+   {'id':11,'title':'AsyncTask和Handler的比较','description':'AsyncTask和Handler的比较','time':'2016-02-15','path':'android/AsyncTask和Handler的比较.md'},
+   {'id':11,'title':'Android动画','description':'Android动画','time':'2016-02-16','path':'android/Animation.md'}
+
  ];
 ARTICLES = resetArticle();
 
